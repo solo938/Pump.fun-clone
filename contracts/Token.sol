168a17b9b04c34237555702faafddb1008285436
Sahariar Hasan <68711966+solo938@users.noreply.github.com>
2025-02-14 20:44:34 +0530

    Update Token.sol

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,1 +1,23 @@
-ss
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Token is ERC20, Ownable {
+    address public creator;
+
+    constructor(
+        address _creator,
+        string memory _name,
+        string memory _symbol,
+        uint256 _totalSupply
+    ) ERC20(_name, _symbol) {
+        creator = _creator;
+        _mint(_creator, _totalSupply);
+    }
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+}
